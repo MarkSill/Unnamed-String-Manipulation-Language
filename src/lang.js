dad6001b4591ff2f5c283847e1793e29bce2579b
MarkSill <marksillweb@gmail.com>
2017-04-08 11:54:29 -0700

    Added "debugger"

--- src/lang.js
+++ src/lang.js
@@ -4,41 +4,77 @@ var Lang;
 	"use strict";
 
 	Lang = {
-		run: (code = "", input = "") => {
+		run: (code = "", input = "", debug = undefined) => {
 			if (!code) {
 				code = "h";
 			}
 			let output = "";
 			let str;
+			let nStr;
 			let index = 1;
 			let escape = false;
 			let stack = [];
 			let cmd = (buffer) => {
-				let func = stack.shift();
-				let temp = (func || Lang.commandDefault)(buffer, input);
-				if (typeof temp === "string") {
-					output += temp;
+				stack.push({
+					cmd: Lang.commandDefault,
+					index: index
+				});
+				oldCmd(buffer);
+			};
+			let oldCmd = (buffer) => {
+				let func = stack.pop();
+				buffer = func.cmd(buffer, input);
+				if (debug) {
+					debug(output + buffer, func.index - 1, func.lastIndex || func.index);
+				}
+				if (stack.length) {
+					oldCmd(buffer);
+				} else {
+					output += buffer;
 				}
 				return output;
 			};
 			for (let ch of code) {
-				if (typeof str !== "string") {
+				if (nStr) {
+					if (!isNaN(parseInt(ch))) {
+						nStr += ch;
+					} else {
+						let n = parseInt(nStr);
+						if (debug) {
+							debug(output, index - nStr.length - 1, index);
+						}
+						nStr = undefined;
+					}
+				}
+				if (typeof str !== "string" && !nStr) {
 					if (ch === "h") {
 						cmd("Hello, world!");
 					} else if (ch === "u") {
-						stack.push(Lang.commandUpper);
+						stack.push({
+							cmd: Lang.commandUpper,
+							index: index
+						});
 					} else if (ch === "l") {
-						stack.push(Lang.commandLower);
+						stack.push({
+							cmd: Lang.commandLower,
+							index: index
+						});
 					} else if (ch === "\"") {
 						str = "";
 					} else if (ch === "'") {
 						str = " ";
+					}
+					else if (!isNaN(parseInt(ch))) {
+						nStr = ch;
 					}
 					else if (ch === "\n" || ch === "\t" || ch === " ") {
 						//do nothing
 					} else {
 						output = `Error: Invalid command "${ch}" at character ${index}.`;
 						break;
+					}
+					if (debug && typeof str !== "string" && !nStr) {
+						//debug(output, index - 1, index);
 					}
 				} else {
 					let c = ch;
@@ -54,10 +90,20 @@ var Lang;
 						if (c === "\\") {
 							escape = true;
 						} else if (c === "\"") {
-							cmd(str);
+							stack.push({
+								cmd: Lang.commandDefault,
+								index: index - str.length - 1,
+								lastIndex: index
+							});
+							oldCmd(str);
 							str = undefined;
 						} else if (c === "'") {
-							cmd(`${str} `);
+							stack.push({
+								cmd: Lang.commandDefault,
+								index: index - str.length - 1,
+								lastIndex: index
+							});
+							oldCmd(`${str} `);
 							str = undefined;
 						} else {
 							str += c;
@@ -67,7 +113,14 @@ var Lang;
 				index++;
 			}
 			if (str) {
-				cmd(str);
+				stack.push({
+					cmd: Lang.commandDefault,
+					index: index - str.length - 1,
+					lastIndex: index
+				});
+				oldCmd(str);
+			} else if (nStr) {
+				cmd(parseInt(nStr));
 			}
 			return output;
 		},
--- src/main.js
+++ src/main.js
@@ -5,8 +5,65 @@
 	const input = document.querySelector("#input");
 	const output = document.querySelector("#output");
 	const button = document.querySelector("#submit");
+	const step = document.querySelector("#step");
+	const stepFirst = document.querySelector("#code-step-first");
+	const stepMiddle = document.querySelector("#code-step-middle");
+	const stepLast = document.querySelector("#code-step-last");
+	const stepUnder = document.querySelector("#code-step-under");
+
+	let steps;
+	let stepIndex;
+	let stepResult;
+
+	code.addEventListener("input", () => {
+		steps = undefined;
+		stepFirst.innerText = "";
+		stepMiddle.innerText = "";
+		stepLast.innerHTML = "&nbsp;";
+		stepUnder.innerHTML = "&nbsp;";
+	});
 
 	button.addEventListener("click", () => {
 		output.innerText = Lang.run(code.value, input.value);
+	});
+
+	step.addEventListener("click", () => {
+		if (!steps) {
+			steps = [];
+			stepIndex = 0;
+			stepResult = Lang.run(code.value, input.value, (currentOutput, index, endIndex) => {
+				steps.push({
+					output: currentOutput,
+					index: index,
+					endIndex: endIndex
+				});
+			});
+		}
+		let s = steps[stepIndex++];
+		if (s) {
+			stepFirst.innerText = code.value.substring(0, s.index);
+			stepMiddle.innerText = code.value.substring(s.index, s.endIndex);
+			stepLast.innerText = code.value.substring(s.endIndex);
+			let str = "";
+			for (let i = 0; i < s.index; i++) {
+				str += "&nbsp;";
+			}
+			let size = s.endIndex - s.index - 1;
+			if (size === 0) {
+				size = 1;
+			}
+			for (let i = 0; i < size; i++) {
+				str += "^";
+			}
+			stepUnder.innerHTML = str;
+			output.innerText = s.output;
+		} else {
+			stepFirst.innerText = code.value;
+			stepMiddle.innerText = "";
+			stepLast.innerHTML = "&nbsp;";
+			stepUnder.innerHTML = "&nbsp;";
+			steps = undefined;
+			output.innerText = stepResult;
+		}
 	});
 })();
